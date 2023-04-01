0ad3717d5c2c816ebd23dc35f0aa3fdf30a3f516
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-04-01 22:42:17 +0200

    v2

--- doc/DISCLAIMER_fr.md
+++
@@ -1,4 +0,0 @@
-#### Support multi-utilisateur
-
-* L'application ne supporte pas le SSO/LDAP.
-* L'application peut être utilisées par plusieurs utilisateurs.
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? YNH_PHP_VERSION="7.4"
-
-#REMOVEME? pkg_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-xml"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
