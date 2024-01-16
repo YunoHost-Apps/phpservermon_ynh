2cf88ba3d21e8f1358b240daea70874b4acc9984
Félix Piédallu <felix@piedallu.me>
2024-01-16 21:51:49 +0100

    Manifest v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
-
-pkg_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-xml"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
